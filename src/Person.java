ac5adde1a695aa696fbcac6dc85adaf5b3f754aa
123 <chap7@mail.ru>
2022-11-20 14:07:27 +0300

    ДЗ. Объекты. 2 курс

---
+++ src/Person.java
@@ -0,0 +1,37 @@
+public class Person {
+    private int yearOfBirth;
+    private String name;
+    private String town;
+    private String jobTitle;
+
+    public Person(int yearOfBirth, String name, String jobTitle, String town) {
+        String defaultValue = "Информация не указана";
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        }
+        if (name == null || name == "") {
+            this.name = defaultValue;
+        } else {
+            this.name = name;
+        }
+        if (jobTitle == null || jobTitle == "") {
+            this.jobTitle = defaultValue;
+        } else {
+            this.jobTitle = jobTitle;
+        }
+        if (town == null || town == "") {
+            this.town = defaultValue;
+        } else {
+            this.town = town;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + name + "! Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности - " + jobTitle + ". Будем знакомы!";
+    }
+
+}
